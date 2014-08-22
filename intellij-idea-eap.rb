bb6bfa28b4c808841a30c06d43b7823e812bf1a2
mdodsworth <dodster.michael@gmail.com>
2014-08-22 15:17:33 -0700

    initial commit -- adding intellij eap

---
+++ intellij-idea-eap.rb
@@ -0,0 +1,9 @@
+class IntellijIdeaEap < Cask
+  version '138.1696'
+  sha256 '747f41b3074a2f277197f4af3599b52cd62803d55e271b146cbb1a98cabdf609'
+
+  url 'http://download.jetbrains.com/idea/ideaIU-138.1696.2.dmg'
+  homepage 'https://www.jetbrains.com/idea/index.html'
+
+  link 'IntelliJ IDEA 14 EAP.app'
+end
