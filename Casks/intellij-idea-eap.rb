0acc659f43b1edd0c5774f0484fd21efd0ed21a6
mdodsworth <mdodsworth@salesforce.com>
2014-09-23 12:20:49 -0700

    updating sha for eap

--- Casks/intellij-idea-eap.rb
+++ Casks/intellij-idea-eap.rb
@@ -1,6 +1,6 @@
 class IntellijIdeaEap < Cask
   version 'ideaIU-138.2210.3'
-  sha256 '8febe686b5dc354e0885466d75945dd37b699df21d118391ba4c2fd3df264487f' 
+  sha256 'febe686b5dc354e0885466d75945dd37b699df21d118391ba4c2fd3df264487f'
 
   url 'http://download.jetbrains.com/idea/ideaIU-138.2210.3.dmg'
   homepage 'https://www.jetbrains.com/idea/index.html'
