dbfad85353158d1f23382ff5066765202a6b8f26
mdodsworth <mdodsworth@salesforce.com>
2014-09-22 17:16:49 -0700

    fixing the shasum for intellij

--- Casks/intellij-idea-eap.rb
+++ Casks/intellij-idea-eap.rb
@@ -1,6 +1,6 @@
 class IntellijIdeaEap < Cask
   version '138.1980.1'
-  sha256 '747f41b3074a2f277197f4af3599b52cd62803d55e271b146cbb1a98cabdf609'
+  sha256 '884e36d3698b22580bb5269c1c29ef18c2fdd7b17c498676ae3a109d3ff67817'
 
   url 'http://download.jetbrains.com/idea/ideaIU-138.1980.1.dmg'
   homepage 'https://www.jetbrains.com/idea/index.html'
