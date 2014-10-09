5eaf448d4c508b31957d1b1ea4d843cf4b7a887b
mdodsworth <mdodsworth@salesforce.com>
2014-10-09 11:56:28 -0700

    fixing the url

--- Casks/intellij-idea-eap.rb
+++ Casks/intellij-idea-eap.rb
@@ -2,7 +2,7 @@ class IntellijIdeaEap < Cask
   version 'ideaIU-139.1'
   sha256 'aec42e7e384ec6bed1fdae5fcc4bdc6c8a9866a7b175bbbe5c1ee1b2a648630b'
 
-  url 'http://download.jetbrains.com/idea/ideaIU-138.2210.3.dmg'
+  url 'http://download.jetbrains.com/idea/ideaIU-139.1.20.dmg'
   homepage 'https://www.jetbrains.com/idea/index.html'
 
   link 'IntelliJ IDEA 14 EAP.app'
