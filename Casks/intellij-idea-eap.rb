cf2e98825512a4a6f936816c3158ea7504f5f1bd
mdodsworth <dodster.michael@gmail.com>
2014-10-23 10:29:00 -0700

    updating shasum

--- Casks/intellij-idea-eap.rb
+++ Casks/intellij-idea-eap.rb
@@ -1,6 +1,6 @@
 class IntellijIdeaEap < Cask
   version 'ideaIU-139.144.2'
-  sha256 'aec42e7e384ec6bed1fdae5fcc4bdc6c8a9866a7b175bbbe5c1ee1b2a648630b'
+  sha256 '49ef8ac753287031ae3ed604bbfd7a19e46359b318ac581db0a6100fb5ae0beb'
 
   url 'http://download.jetbrains.com/idea/ideaIU-139.144.2.dmg'
   homepage 'https://www.jetbrains.com/idea/index.html'
