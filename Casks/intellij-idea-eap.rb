10796caa2ec6584ff2bd289c5e4143823346e178
mdodsworth <mdodsworth@salesforce.com>
2014-10-23 10:18:49 -0700

    upgrading to the latest

--- Casks/intellij-idea-eap.rb
+++ Casks/intellij-idea-eap.rb
@@ -1,8 +1,8 @@
 class IntellijIdeaEap < Cask
-  version 'ideaIU-139.1'
+  version 'ideaIU-139.144.2'
   sha256 'aec42e7e384ec6bed1fdae5fcc4bdc6c8a9866a7b175bbbe5c1ee1b2a648630b'
 
-  url 'http://download.jetbrains.com/idea/ideaIU-139.1.20.dmg'
+  url 'http://download.jetbrains.com/idea/ideaIU-139.144.2.dmg'
   homepage 'https://www.jetbrains.com/idea/index.html'
 
   link 'IntelliJ IDEA 14 EAP.app'
