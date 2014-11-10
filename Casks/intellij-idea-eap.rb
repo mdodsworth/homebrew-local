81ee8c302bea2ffb1b945701b5a46f82c13ad617
mdodsworth <dodster.michael@gmail.com>
2014-11-10 10:36:01 -0800

    idea eap update

--- Casks/intellij-idea-eap.rb
+++ Casks/intellij-idea-eap.rb
@@ -1,8 +1,8 @@
 class IntellijIdeaEap < Cask
-  version 'ideaIU-139.144.2'
+  version 'ideaIU-139.224'
   sha256 '49ef8ac753287031ae3ed604bbfd7a19e46359b318ac581db0a6100fb5ae0beb'
 
-  url 'http://download.jetbrains.com/idea/ideaIU-139.144.2.dmg'
+  url 'http://download.jetbrains.com/idea/ideaIU-14.dmg'
   homepage 'https://www.jetbrains.com/idea/index.html'
 
   link 'IntelliJ IDEA 14 EAP.app'
