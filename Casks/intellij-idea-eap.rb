b34cce91eb4b17402c9ac53caf7a953d58d3017b
mdodsworth <michael.dodsworth@optimizely.com>
2015-01-05 20:24:10 +0000

    removing defunct intellij eap brew

--- Casks/intellij-idea-eap.rb
+++
@@ -1,9 +0,0 @@
-class IntellijIdeaEap < Cask
-  version 'ideaIU-139.224'
-  sha256 '9695bedfe145d70010b853cc67d7dc07bf2768e38b5b9034d1f6a00b0b9a15a7'
-
-  url 'http://download.jetbrains.com/idea/ideaIU-14.dmg'
-  homepage 'https://www.jetbrains.com/idea/index.html'
-
-  link 'IntelliJ IDEA 14.app'
-end
