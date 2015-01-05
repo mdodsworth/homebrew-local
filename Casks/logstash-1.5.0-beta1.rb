30eb6d61144eceb2d34adc89a599903482127e5b
mdodsworth <michael.dodsworth@optimizely.com>
2015-01-05 20:28:42 +0000

    adding logstash beta brew

---
+++ Casks/logstash-1.5.0-beta1.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+class Logstash < Formula
+  homepage 'http://logstash.net/'
+  url 'http://download.elasticsearch.org/logstash/logstash/logstash-1.5.0.beta1.tar.gz'
+  sha1 '963086de61d418b2da4fab79a165e40c67fabded'
+
+  def install
+    inreplace %w{bin/logstash bin/logstash.lib.sh}, /^basedir=.*$/, "basedir=#{libexec}"
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/logstash"
+  end
+
+  def caveats; <<-EOS.undent
+    Logstash 1.4+ emits an unhelpful error if you try to start it without config.
+    Please read the getting started guide located at:
+    http://logstash.net/docs/latest/tutorials/getting-started-with-logstash
+    EOS
+  end
+
+  test do
+    system "#{bin}/logstash", "--version"
+    system "/bin/echo 'hello world' | #{bin}/logstash agent -e 'input { stdin { type => stdin } } output { stdout { codec => rubydebug } }'"
+  end
+end
